4e04a01ac086677063263ce60b9d1ffcd955ef81
Elmahdi Abdallh <almhdybdallh24@gmail.com>
2026-05-31 21:47:44 +0200

    Fix Android Gradle config: remove explicit plugin version, use Flutter injection

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -3,12 +3,6 @@ pluginManagement {
         google()
         mavenCentral()
         gradlePluginPortal()
-        maven {
-            url = uri("https://storage.googleapis.com/download.flutter.io")
-        }
-    }
-    plugins {
-        id("dev.flutter.flutter-gradle-plugin") version "1.0.0"
     }
 }
 
